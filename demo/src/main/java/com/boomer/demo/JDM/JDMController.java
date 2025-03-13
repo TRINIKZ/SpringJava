3d8dd1bb97477c0b8ef5b2eca0db8f934228f8ca
Victor Feitosa <88910141+TRINIKZ@users.noreply.github.com>
2025-03-13 19:59:29 -0300

    Add files via upload

--- demo/src/main/java/com/boomer/demo/JDM/JDMController.java
+++ demo/src/main/java/com/boomer/demo/JDM/JDMController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.mindrot.jbcrypt.BCrypt;
+
 
 @RestController
 @RequestMapping("/jdm")
@@ -22,17 +24,41 @@ private ResponseEntity JDMModel(@RequestBody JDMModel jdmModel, HttpServletReque
     var existente = this.jdmRepository.findByModelo(jdmModel.getModelo());
 
     if (existente != null) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("cadastro existente");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body:"cadastro existente");
         
-    } else {
+    } /*else {
         var criado = this.jdmRepository.save(jdmModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(criado);
-        
+        */
+                // 13/03
+            else {
+            var senhaHash = BCrypt.withDefaults()
+                .hashToString(cost:12, userModel.getSenha().toCharArray());
+                userModel.setSenha(senhaHash);
+                var criado = this.jdmRepository.save(jdmModel);
+                return ResponseEntity.status(HttpStatus.CREATED).body(criado)
     }
 }
 
 @GetMapping("/mensagem")
 private String retornoMensagem(){
     return "Você acessou um link secreto!";
+}
+// 13/03
+@GetMapping("/jdmusers")
+public List<UserModel> listarCarros() {
+    List<UserModel> carrocad = jdmRepository.findAll();
+    return carrocad;
+}
+
+@PutMapping("/atualiza")
+public ResponseEntity atualizaUser(@RequestBody UserModel usermodel){
+    var criado = this.jdmRepository.save(userModel);
+    return Responseentity.status(HttpStatus.CREATED).body(criado);
+}
+
+@DeleteMapping("/deletauser/{id}")
+public void deletaUser(@PathVariable UUID id){
+    jdmRepository.deleteById(id);
 }
 }
